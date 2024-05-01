07a40c36e5ed7f19b1fd148c0843ae08c55449f0
nuharaomesh <omeshnuhara4@gmail.com>
2024-05-01 11:50:46 +0530

    done apply SPA

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-$("")
+
+$("#customer-section").css({display: "none"});
+$("#order-section").css({display: "none"});
+
+$("#nav-item").on('click', () => {
+    $("#item-section").css({display: "block"});
+    $("#customer-section").css({display: "none"});
+    $("#order-section").css({display: "none"});
+});
+$("#nav-orders").on('click', () => {
+    $("#order-section").css({display: "block"});
+    $("#customer-section").css({display: "none"});
+    $("#item-section").css({display: "none"});
+});
+$("#nav-customer").on('click', () => {
+    $("#customer-section").css({display: "block"});
+    $("#order-section").css({display: "none"});
+    $("#item-section").css({display: "none"});
+});
+
